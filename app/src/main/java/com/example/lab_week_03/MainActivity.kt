399736c3a4a219658237a773532b8237b41146d8
Rafi Ghadhanfar <rafighadhanfarmuhammad@gmail.com>
2025-09-09 16:33:26 +0700

    Commit No. 3 – use dynamic fragment

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
 
 interface CoffeeListener {
     fun onSelected(id: Int)
@@ -15,12 +16,21 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         Log.d(TAG, "onCreate")
+        if(savedInstanceState == null){
+            findViewById<FragmentContainerView>(R.id.fragment_container).let{
+                containerLayout ->
+                    val listFragment = ListFragment()
+                    supportFragmentManager.beginTransaction()
+                        .add(containerLayout.id, listFragment)
+                        .commit()
+            }
+        }
     }
 
     override fun onStart() {
@@ -44,12 +54,20 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
     }
 
     override fun onSelected(id: Int){
-        val detailFragment = supportFragmentManager
+        /*val detailFragment = supportFragmentManager
             .findFragmentById(R.id.fragment_detail) as DetailFragment
-        detailFragment.setCoffeeData(id)
+        detailFragment.setCoffeeData(id)*/
+        findViewById<FragmentContainerView>(R.id.fragment_container).let {
+            containerLayout -> val detailFragment = DetailFragment.newInstance(id)
+            supportFragmentManager.beginTransaction()
+                .replace(containerLayout.id, detailFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     companion object {
         private const val TAG = "MainActivity"
+
     }
 }
